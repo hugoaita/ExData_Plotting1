ceaedceeb88f133f7ab7fc0d486272138ecf5f9a
Hugo Aita <hugoaita@gmail.com>
2019-05-24 06:09:23 -0300

    Made changes in time subsetting

--- plot1.R
+++ plot1.R
@@ -8,28 +8,25 @@ if(!file.exists(filename)) {
   unzip(zipfile="getdata_dataset.zip",exdir="./")
 }
 
+# Read to get headers
+initial <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", nrows=5)
+# 2900 is not exactly the number of rows, we will subset them later
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";",
+                   skip=66630, nrows=2900, col.names=names(initial), na.strings=c("?"),
+                   colClasses = c("character", "character", "numeric", "numeric", "numeric",
+                                  "numeric", "numeric", "numeric", "numeric"))
 
-t0 <- as.POSIXct("16/12/2006 17:24:00", format="%d/%m/%Y %H:%M:%S")  
-t1 <- as.POSIXct("01/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S")
-t2 <- as.POSIXct("02/02/2007 23:59:00", format="%d/%m/%Y %H:%M:%S")  
-
-min1 <- difftime(t1, t0, units="mins") + 1
-min2 <- difftime(t2, t0, units="mins") + 1
-
-header <- read.table("household_power_consumption.txt", nrows=1,
-                     header=FALSE, sep=";", stringsAsFactors = FALSE)
-data <- read.table("household_power_consumption.txt", sep=";",
-                   header=FALSE, skip=min1, nrows=min2 - min1 + 1)
-colnames(data) <- unlist(header)
-
+# Convert Date & Time variables to Date/Time
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
-data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
-data$Global_reactive_power <- as.numeric(as.character(data$Global_reactive_power))
-data$Voltage <- as.numeric(as.character(data$Voltage))
+data$Time <- strptime(paste(data$Date, data$Time), "%F %T")
 
-data <- transform(data, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+# Subset the correct number of rows
+data <- subset(data, data$Date %in% as.Date(c("2007-02-01", "2007-02-02")))
+
 
 png(file="plot1.png", width=480, height=480)
+# I add this row because if we run 'plot4.R' first, we will get a graph with
+# four subplots
 par(mfrow=c(1,1)) 
 hist(data$Global_active_power, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
 title(main="Global Active Power")
--- plot4.R
+++ plot4.R
@@ -8,49 +8,37 @@ if(!file.exists(filename)) {
   unzip("getdata_dataset.zip")
 }
 
-data <- read.table("household_power_consumption.txt", sep=";",header=TRUE, nrows=1)
-data$Date <- as.Date(data$Date, "%d/%m/%Y")
+# Read to get headers
+initial <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", nrows=5)
+# 2900 is not exactly the number of rows, we will subset them later
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";",
+                   skip=66630, nrows=2900, col.names=names(initial), na.strings=c("?"),
+                   colClasses = c("character", "character", "numeric", "numeric", "numeric",
+                                  "numeric", "numeric", "numeric", "numeric"))
 
-t0 <- as.POSIXct("16/12/2006 17:24:00", format="%d/%m/%Y %H:%M:%S")  
-t1 <- as.POSIXct("01/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S")
-t2 <- as.POSIXct("02/02/2007 23:59:00", format="%d/%m/%Y %H:%M:%S")  
-
-min1 <- difftime(t1, t0, units="mins") + 1
-min2 <- difftime(t2, t0, units="mins") + 1
-
-header <- read.table('household_power_consumption.txt', nrows=1,
-                     header=FALSE, sep=";", stringsAsFactors = FALSE)
-data <- read.table("household_power_consumption.txt", sep=";",
-                   header=FALSE, skip=min1, nrows=min2 - min1 + 1)
-colnames(data) <- unlist(header)
-
+# Convert Date & Time variables to Date/Time
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
-data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
-data$Global_reactive_power <- as.numeric(as.character(data$Global_reactive_power))
-data$Voltage <- as.numeric(as.character(data$Voltage))
+data$Time <- strptime(paste(data$Date, data$Time), "%F %T")
 
+# Subset the correct number of rows
+data <- subset(data, data$Date %in% as.Date(c("2007-02-01", "2007-02-02")))
 
-data <- transform(data, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
-
-data$Sub_metering_1 <- as.numeric(as.character(data$Sub_metering_1))
-data$Sub_metering_2 <- as.numeric(as.character(data$Sub_metering_2))
-data$Sub_metering_3 <- as.numeric(as.character(data$Sub_metering_3))
 
 
 png(file="plot4.png", width = 480, height = 480)
 par(mfrow=c(2,2))
-plot(data$timestamp, data$Global_active_power, type="l", xlab="", 
+plot(data$Time, data$Global_active_power, type="l", xlab="", 
      ylab="Global Active Power")
-plot(data$timestamp, data$Voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(data$Time, data$Voltage, type="l", xlab="datetime", ylab="Voltage")
 
-plot(data$timestamp, data$Sub_metering_1, type="l", xlab="", ylab="Energy submetering")
-lines(data$timestamp, data$Sub_metering_2, col="red")
-lines(data$timestamp, data$Sub_metering_3, col="blue")
+plot(data$Time, data$Sub_metering_1, type="l", xlab="", ylab="Energy submetering")
+lines(data$Time, data$Sub_metering_2, col="red")
+lines(data$Time, data$Sub_metering_3, col="blue")
 legend("topright", lty=1, lwd=1, bty="n", col=c("black", "red","blue"), 
        c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
        cex=0.6)
 
-plot(data$timestamp, data$Global_reactive_power, type="l", xlab="datetime", 
+plot(data$Time, data$Global_reactive_power, type="l", xlab="datetime", 
      ylab="Global Reactive Power")
 
 
